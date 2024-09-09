8ed05b8c7fa1d615477fe514c221147325d5ac6d
weishitong <571443750@qq.com>
2024-09-09 21:23:05 +0800

    保护性暂停模式的提交

--- src/main/java/com/thread/GuardedObject/GuaredeObject.java
+++ src/main/java/com/thread/GuardedObject/GuaredeObject.java
@@ -4,21 +4,31 @@ public class GuaredeObject {
     //结果
     private Object response;
 
-    //获取结果
-    public Object get(){
+    /**
+     * 获取结果
+     * @param timeout 表示要等待多久
+     * @return
+     */
+    public Object get(long timeout){
         synchronized (this){
-            //没有结果
-           while(response == null){
-               try {
-                   this.wait();
-               } catch (InterruptedException e) {
-                   e.printStackTrace();
-               }
-           }
+            long begin = System.currentTimeMillis();
+            long passedTime = 0;
+            while(response == null){
+                long waitTime = timeout - passedTime;
+                if(waitTime <= 0){
+                    break;
+                }
+                try {
+                    this.wait(waitTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                passedTime = System.currentTimeMillis() - begin;
+            }
            return response;
         }
     }
-    
+
     //产生结果
     public void complete(Object response){
         synchronized (this){
--- src/main/java/com/thread/GuardedObject/test.java
+++ src/main/java/com/thread/GuardedObject/test.java
@@ -1,4 +1,29 @@
 package com.thread.GuardedObject;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
 public class test {
+    public static void main(String[] args) {
+        GuaredeObject guaredeObject = new GuaredeObject();
+        new Thread(() -> {
+            log.debug("begin");
+            Object response = guaredeObject.get(2000);
+            log.debug("结果是:{}", response);
+        }, "t1").start();
+
+        new Thread(() -> {
+            log.debug("begin");
+            try {
+                Thread.sleep(1000);
+                guaredeObject.complete(null);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"t2").start();
+    }
 }
